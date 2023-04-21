aa7cb95e3cfa1334f7fdb3762cc5ebd8e6961f94
BeqaShevardenidze <beqa.shevardenidze968@gmail.com>
2023-04-21 12:08:50 +0400

    add files

---
+++ js/js.js
@@ -0,0 +1,79 @@
+function f1(){
+    document.getElementById("resultId").innerHTML = "1";
+}
+
+function f2(){
+    document.getElementById("resultId").innerHTML = "2";
+}
+
+function f3(){
+    document.getElementById("resultId").innerHTML = "3";
+}
+
+function f4(){
+    document.getElementById("resultId").innerHTML = "*";
+}
+
+function f5(){
+    document.getElementById("resultId").innerHTML = "4";
+}
+
+function f6(){
+    document.getElementById("resultId").innerHTML = "5";
+}
+
+function f7(){
+    document.getElementById("resultId").innerHTML = "6";
+}
+
+function f8(){
+    document.getElementById("resultId").innerHTML = "+";
+}
+
+function f9(){
+    document.getElementById("resultId").innerHTML = "7";
+}
+
+function f10(){
+    document.getElementById("resultId").innerHTML = "8";
+}
+
+function f11(){
+    document.getElementById("resultId").innerHTML = "9";
+}
+
+function f12(){
+    document.getElementById("resultId").innerHTML = "-";
+}
+
+function f13(){
+    document.getElementById("resultId").innerHTML = ".";
+}
+
+function f14(){
+    document.getElementById("resultId").innerHTML = "0";
+}
+
+function f15(){
+    document.getElementById("resultId").innerHTML = "=";
+}
+
+function f16(){
+    document.getElementById("resultId").innerHTML = "Reset";
+}
+
+function f17(){
+    document.getElementById("resultId").innerHTML = "Del";
+}
+
+function f18(){
+    document.getElementById("resultId").innerHTML = "%";
+}
+
+function f19(){
+    document.getElementById("resultId").innerHTML = "/";
+}
+
+var udris = "=";
+alert(typeof (udris));
+alert(parseInt);
